3b67d848a865925d904fd23ee548d896741dd322
bambucha <bambucha@bnotebook>
2011-05-03 23:15:49 +0400

    Набросок

---
+++ src/jeditor/DBEditConnector.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jeditor;
+
+/**
+ *
+ * @author bambucha
+ */
+public class DBEditConnector {
+    
+}
